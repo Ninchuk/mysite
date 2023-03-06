de6e0758e397cb5c5eb88b0f4a6a392777f9bd1a
Ninchuk <mudgtt@gmail.com>
2023-03-06 14:50:59 +0300

    add searching with trigram

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 class PostListView(ListView):
@@ -88,7 +88,6 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
-            search_query = SearchQuery(query)
-            results = Post.published.annotate(search_vector, search_query).filter(rank__gte=0.3).order_by('-rank')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query),
+                                              ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
