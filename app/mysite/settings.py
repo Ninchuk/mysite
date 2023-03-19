942a10fcf7dca0b4ed75aeba00f98a2ecfbfba5e
Iceman <mudgtt@gmail.com>
2023-03-19 16:57:33 +0300

    add email

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -185,3 +185,9 @@ LOGGING = {
         },
     },
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mudgtt@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
